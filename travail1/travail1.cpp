413ab6740e0dd87aa94109e107f1f7588320573a
Yoann BARBIN <1831983@cegepba.qc.ca>
2020-02-06 15:10:00 -0500

    Partie 3 - Nom des joueurs (debut de code initialiser joueur)

--- travail1/travail1.cpp
+++ travail1/travail1.cpp
@@ -9,22 +9,20 @@ using namespace std;
 
 Donnees leJeu;
 
-void InitialiserJoueurs();
+void InitialiserJoueurs(Joueur* Joueur1, Joueur* Joueur2);
 void Jouer();
 
 int main() 
 {
 
-	
-
-
-
 	char fini = 'n';
 	srand((unsigned)time(NULL));
-	InitialiserJoueurs();
+	InitialiserJoueurs(&leJeu.Joueur1, &leJeu.Joueur2);
 	while (fini=='n' || fini=='N')
 	{
-		Jouer();
+		Jouer(); 
+		cout << leJeu.Joueur1.getNom();
+		cout << leJeu.Joueur2.getNom();
 		cout << "\nTermine (n/o) ? ";
 		cin >> fini;		
 	}
@@ -32,9 +30,22 @@ int main()
 
 void Jouer() 
 {
-
+	
 }
 
-void InitialiserJoueurs()
+void InitialiserJoueurs(Joueur* Joueur1, Joueur* Joueur2)
 {
+	ClrScr();
+
+	string nomJoueur1;
+	string nomJoueur2;
+
+		cout << "Joueur1, entrez votre nom :";
+		cin >> nomJoueur1;
+		Joueur1->setNom(nomJoueur1);
+
+		cout << "Joueur2, entrez votre nom :";
+		cin >> nomJoueur2;
+		Joueur2->setNom(nomJoueur2);
+
 }
