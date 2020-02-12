fa0c1b22404293325df8b9de983c41ecaaa6c828
Yoann BARBIN <1831983@cegepba.qc.ca>
2020-02-12 16:50:49 -0500

    Finalisation du travail 1 - Ajouter Victoire/Defaite - Grand Gagnant - Recommencer partie - Afficher Statistiques.

--- travail1/travail1.cpp
+++ travail1/travail1.cpp
@@ -5,16 +5,24 @@
 #include <iostream>
 #include <iomanip>
 #include <time.h>
+
 using namespace std;
+
+
 
 Donnees leJeu;
 
 void InitialiserJoueurs(Joueur* Joueur1, Joueur* Joueur2);
+void AfficherLesJoueurs();
 void Jouer();
-int DemanderNombreDeCartes();
+ int DemanderNombreDeCartes();
 void MelangerCartes();
 void DistribuerLesCartes(int MaxCarteADistribuer);
-void AfficherLesCartes();
+ int AfficherLesCartes(int MaxCarteADistribuer, Joueur InJoueur);
+ void AfficherGagnant(int TotalJoueur1, int TotalJoueur2);
+ void AfficherGrandGagnant();
+
+
 
 
 int main() 
@@ -27,28 +35,18 @@ int main()
 	while (fini=='n' || fini=='N')
 	{
 		Jouer();
-		
-		
 	//   Jouer(); // Jouer plus haut que cout
-	//cout << "\nTermine (n/o) ? ";
-	//cin >> fini;		
+	cout << "\nTermine (n/o) ? ";
+	cin >> fini;	
+
+	AfficherGrandGagnant();
 	}
 }
 
-void Jouer() 
-{
-	int MaxCartesADistribuer = 0;
+//While -» rentre un chiffre entre 1 et 26
 
-	ClrScr();
 
-	
-	//AfficherLesJoueurs();
-	MaxCartesADistribuer = DemanderNombreDeCartes();
-	MelangerCartes();
 
-	DistribuerLesCartes(MaxCartesADistribuer);
-	
-}
 
 void InitialiserJoueurs(Joueur* Joueur1, Joueur* Joueur2)
 {
@@ -67,11 +65,69 @@ void InitialiserJoueurs(Joueur* Joueur1, Joueur* Joueur2)
 
 }
 
+
+
+
+
 void AfficherLesJoueurs()
+
 {
 	cout << "\nJoueur 1 :\n" << leJeu.Joueur1.getNom() << "\n";
 	cout << "\nJoueur 2 : \n" << leJeu.Joueur2.getNom() << "\n";
 }
+
+
+
+
+
+void Jouer() 
+{
+	int MaxCarteADistribuer = 0;
+	int TotalJoueur1 = 0;
+	int TotalJoueur2 = 0;
+
+
+	ClrScr();
+
+	AfficherLesJoueurs();
+	MaxCarteADistribuer = DemanderNombreDeCartes();
+	MelangerCartes();
+
+	DistribuerLesCartes(MaxCarteADistribuer);
+
+	TotalJoueur1 = AfficherLesCartes(MaxCarteADistribuer, leJeu.Joueur1);
+	TotalJoueur2 = AfficherLesCartes(MaxCarteADistribuer, leJeu.Joueur2);
+
+	AfficherGagnant(TotalJoueur1, TotalJoueur2);
+
+	leJeu.Joueur1.RetirerCartes();
+	leJeu.Joueur2.RetirerCartes();
+
+}
+
+
+
+
+int DemanderNombreDeCartes()
+{
+
+
+	int MaxCarteADistribuer = 0;
+
+
+	cout << "\nCombien de cartes a distribuer ? (Max 26)\n";
+	cin >> MaxCarteADistribuer;
+	cout << "Nombre de cartes a distribuer :\n" << MaxCarteADistribuer << "\n\n";
+	
+	return MaxCarteADistribuer;
+
+		//if (MaxCarteADistribuer > 26)
+		//{
+		//	cout << "Erreur, Maximum de cartes a distribuer : 26";
+		//	Jouer();
+}
+
+
 
 void MelangerCartes()
 {
@@ -102,27 +158,8 @@ void MelangerCartes()
 	//
 	//}
 
-
-
 }
 
-int DemanderNombreDeCartes()
-{
-
-int MaxCarteADistribuer = 0;
-
-
-	cout << "\nCombien de cartes a distribuer ? (Max 26)\n";
-	cin >> MaxCarteADistribuer;
-	cout << "Nombre de cartes a distribuer :\n" << MaxCarteADistribuer << "\n\n";
-	
-	return MaxCarteADistribuer;
-
-		//if (MaxCarteADistribuer > 26)
-		//{
-		//	cout << "Erreur, Maximum de cartes a distribuer : 26";
-		//	Jouer();
-}
 
 
 
@@ -130,8 +167,7 @@ int MaxCarteADistribuer = 0;
 void DistribuerLesCartes(int MaxCarteADistribuer)
 {
 	bool ok = true;
-	
-	
+		
 	for (int i = 0; i <= MaxCarteADistribuer*2 ; i++)
 	{
 		if (ok == true)
@@ -144,20 +180,85 @@ void DistribuerLesCartes(int MaxCarteADistribuer)
 		{
 			leJeu.Joueur2.AjouterCartes(&leJeu.PaquetDeCartes[i]);
 			ok = true;
-		}
+		}		
+	}
+}
+
+int AfficherLesCartes(int MaxCarteADistribuer, Joueur InJoueur)
+{
+	int Total = 0;
+
+	cout << InJoueur.getNom() << "\n\n";
+	cout << "Voici les cartes de votre main :\n";
+
+	for (int i = 0; i < MaxCarteADistribuer; i++)
+	{
 		
+		cout << InJoueur.getCarte(i)->GetValeur() << " " << InJoueur.getCarte(i)->GetAtout() << "\n";
+		Total += InJoueur.getCarte(i)->GetValeur();
+
+	}
+
+	cout << "Total : " << Total << "\n\n";
+
+	return Total;
+
+}
+
+void AfficherGagnant(int TotalJoueur1, int TotalJoueur2)
+{
+
+	if (TotalJoueur1 > TotalJoueur2)
+	{
+
+		cout << leJeu.Joueur1.getNom() << "  gagne la partie ! \n\n";
+		leJeu.Joueur1.AjouterVictoire();
+		leJeu.Joueur2.AjouterDefaite();
+
+
+	}
+	else if (TotalJoueur1 == TotalJoueur2)
+	{
+		cout << "\nEgalite !! \n\n";
+	}
+	else
+	{
+		cout << leJeu.Joueur2.getNom() << "  gagne la partie ! \n\n";
+		leJeu.Joueur2.AjouterVictoire();
+		leJeu.Joueur1.AjouterDefaite();
 	}
 
 }
 
-void AfficherLesCartes(int MaxCarteADistribuer)
+void AfficherGrandGagnant()
 {
-	for (int i = 0; i <= MaxCarteADistribuer; i++)
+	ClrScr();
+
+	cout << "Les joueurs etaient : " << leJeu.Joueur1.getNom() << " et " << leJeu.Joueur2.getNom() << "\n\n";
+
+	cout << "Voici les stats du joueur  " << leJeu.Joueur1.getNom() << ": \n\n";
+	cout << "Nombre de victoires : " << leJeu.Joueur1.getNombreDeVictoire() << "\n\n";
+	cout << "Nombre de defaites : " << leJeu.Joueur1.getNombreDeDefaite()  << "\n\n\n";
+
+	cout << "Voici les stats du joueur  " << leJeu.Joueur2.getNom() << ": \n\n";
+	cout << "Nombre de victoires : " << leJeu.Joueur2.getNombreDeVictoire() << "\n\n";
+	cout << "Nombre de defaites : " << leJeu.Joueur2.getNombreDeDefaite() << "\n\n\n";
+
+	if (leJeu.Joueur1.getNombreDeVictoire() > leJeu.Joueur2.getNombreDeVictoire())
 	{
-
-		cout << leJeu.Joueur1.getCarte(i)->GetValeur() << " " << leJeu.Joueur1.getCarte(i)->GetAtout();
-
+		cout << "Le grand gagnant de la partie est : " << leJeu.Joueur1.getNom() << "\n";
 	}
+
+	else if (leJeu.Joueur1.getNombreDeVictoire() == leJeu.Joueur2.getNombreDeVictoire())
+	{
+		cout << "Egalite entre : " << leJeu.Joueur1.getNom() << " et " << leJeu.Joueur2.getNom();
+	}
+	else
+	{
+		cout << "Le grand gagnant de la partie est : " << leJeu.Joueur2.getNom() << "\n";
+	}
+
+	
 
 
 }
